810e136ba8fff2c99a722b0037a8f7f4a4659c4b
Yousri MHAMDI <yousri.mhamdi@teamwillgroup.com>
2022-09-06 15:59:56 +0100

    Day 5: routing.

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -14,7 +14,7 @@
 <div id="container">
     <div id="header">
         <div class="content">
-            <h1><a href="<?php echo url_for('job/index') ?>">
+            <h1><a href="<?php echo url_for('homepage') ?>">
                     <img src="/legacy/images/logo.jpg" alt="Jobeet Job Board" />
                 </a></h1>
 
